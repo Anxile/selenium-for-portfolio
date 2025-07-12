4925499a5e145cfaa52c2a94df6805275e663c3f
Anxile <onebox.chen@outlook.com>
2025-07-11 21:53:37 -0400

    login success testing

---
+++ tests/test_demo.py
@@ -0,0 +1,8 @@
+class TestDemo:
+    def test1(self ):
+        print("testing")
+        assert 1 == 1
+
+    def test2(self):
+        print("hahahahah")
+        assert 1 == 2
---
+++ tests/test_user_login.py
@@ -0,0 +1,36 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture(scope="function")
+def driver():
+    _driver = webdriver.Chrome()
+    _driver.maximize_window()
+    yield _driver
+    _driver.quit()
+
+
+class TestUserLogin:
+    def test_login_success(self, driver):
+        name = "Yihe_mac"
+        password = "123456"
+        expected_path = "/posts"
+
+        driver.get("https://www.bytecho.ca/")
+
+        driver.find_element(By.ID,"login").click()
+        username_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "user_name"))
+        )
+        username_input.send_keys(name)
+        driver.find_element(By.ID, "user_password").send_keys(password)
+        driver.find_element(By.NAME, "commit").click()
+
+        WebDriverWait(driver, 10).until(
+            EC.url_contains(expected_path)
+        )
+
+        assert driver.current_url.endswith(expected_path)
